5fc18eeccde8dd65c9541c10385faeaf6b820f3e
Your Namesifathero <sifathero466@gmail.com>
2024-05-04 16:27:03 +0600

    2nd Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-4xl font-bold'>Knowledge cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
